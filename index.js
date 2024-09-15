ac4f69452566524678828a7655700051dd99ac19
hanconsol <hasfaraj49@gmail.com>
2024-09-14 23:25:31 -0500

    feat:add post

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const upload = multer({dest: "/uploads/"})
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  console.log("body", req.body);
+  console.log("file", req.file);
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
 
+  });
+});
 
 
 const port = process.env.PORT || 3000;
